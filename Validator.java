29f2814178f1757c2179a6a72de50244dbc3c40f
cirque-du-soli <124804282+cirque-du-soli@users.noreply.github.com>
2023-03-03 09:50:25 -0500

    Add files via upload
    Start Validator.java file

---
+++ Validator.java
@@ -0,0 +1,13 @@
+//This is our main Class file, containing all our methods
+//Name & Student ID
+//Name & Student ID
+
+
+public class Validator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
