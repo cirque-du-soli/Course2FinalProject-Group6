8727628d7b07f66721e3e77ffe785bdc2627e412
cirque-du-soli <124804282+cirque-du-soli@users.noreply.github.com>
2023-03-07 04:58:27 -0500

    Added 3 methods. Fixed spelling/formatting.

--- Validator.java
+++ Validator.java
@@ -1,43 +1,77 @@
-//This is our main Class file, containing all our methods
-//Name & Student ID
-//Name & Student ID
-
+//Jing Wei -- Student # xyz
+//Soliloquy Yarrow -- Student # 2342261
 
 public class Validator {
 
 	public static void main(String[] args) {
-		
+
+		// TESTS:
+		System.out.println("the char B is AlphaNum: " + isAlphaNum('B'));
+		System.out.println("the fetchBeforeAt: you@regreat);: " + fetchBeforeAt("you@regreat"));
+		System.out.println("the char B is AlphaNum: " + isAlphaNum('B'));
+		System.out.println("the char B is AlphaNum: " + isAlphaNum('B'));
 
 	}
-	//check if input argument is a letter or a number, if yes return true, not return false
+
+	// check if input argument is a letter or a number, if yes return true, not
+	// return false
 	public static boolean isAlphaNum(char chr) {
-		if(Character.isLetterOrDigit(chr)) {
+		if (Character.isLetterOrDigit(chr)) {
 			return true;
 		}
 		return false;
 	}
-	
-	//check if input argument is a '-','.', or if the boolean argument is true also allow for the underscore (_) to return true, return false otherwise.
-	public static boolean isSpeciaChar(char chr, boolean isUnderscore) {
+
+	// check if input argument is a '-','.', or if the boolean argument is true also
+	// allow for the underscore (_) to return true, return false otherwise.
+	public static boolean isSpecialChar(char chr, boolean isUnderscore) {
 		if (chr == '-' || chr == '.' || (isUnderscore && chr == '_')) {
 			return true;
 		}
 		return false;
 	}
-	
-	//check if input argument is a character allowed in the prefix. return true if the character can be used in the prefix, false otherwise
+
+	// check if input argument is a character allowed in the prefix. return true if
+	// the character can be used in the prefix, false otherwise
 	public static boolean isPrefixChar(char chr) {
-		if(isAlphaNum(chr) || isSpeciaChar(chr, false)) {
+		if (isAlphaNum(chr) || isSpecialChar(chr, true)) { // true input because
+			return true;
+		}
+		return false;
+	}
+
+	public static String fetchAfterAt(String str) {
+		int indexOfAt = str.indexOf('@');
+		return str.substring(indexOfAt + 1);
+	}
+
+	////// Soli, March 6
+
+	// same as isPrefixChar, except NO underscores.
+	public static boolean isDomainChar(char chr) {
+		if (isAlphaNum(chr) || isSpecialChar(chr, false)) {
 			return true;
 		}
 		return false;
 	}
 	
-	public static String fetchAfterAt(String str) {
-		int indexOfAt =  str.indexOf('@');
-		return str.substring(indexOfAt+1);
+	// Check if string contains exactly one '@' character.
+	public static boolean singleAtSign(String str) {
+		int totalAts = 0; 
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '@') {
+				totalAts++;
+			}	
+		}
+		if (totalAts == 1) {
+			return true;
+		}
+		return false; 
 	}
 	
-	
-
+	// Fetch beginning part of email address, before the '@'. Assume only 1 '@' sign.
+	public static String fetchBeforeAt(String str) {
+		int indexOfAt = str.indexOf('@');
+		return str.substring(0, indexOfAt);
+	}
 }
